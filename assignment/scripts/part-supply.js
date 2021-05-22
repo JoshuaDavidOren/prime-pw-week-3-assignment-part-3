17ddd06f3a9ae88a847d4dd6bafc594e1eeb6830
Joshua Oren <joshuadavidoren@gmail.com>
2021-05-22 17:44:56 -0500

    part 3 complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,7 +4,7 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
-let partsNeeded = 40
+let partsNeeded = '40'
 console.log(partsNeeded)
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
@@ -18,18 +18,25 @@ console.log(supplyChanges[1])
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 supplyChanges.pop()
-console.log(11);
+console.log('11');
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-supplyChanges.push(25)
-console.log(supplyChanges); 
+supplyChanges.push('25')
+
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, skip it. Do not log it to the console.
 //    - if the value is negative, format the log as 'Part count -x.'
 console.log('6. Showing supplyChanges...');
+for (supplyChanges of supplyChanges)
+if( supplyChanges >= '1' ){
+console.log('Added',+ supplyChanges + ' parts');}
+else if ( supplyChanges < '0'){
+console.log('Part count ', supplyChanges);
+}
+
 
 
 // STRETCH GOALS
